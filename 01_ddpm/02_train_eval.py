18e12b3bf6e7a60f94cbfc76247b7d48ad63c863
yuan.feng <54296281@qq.com>
2024-07-24 17:59:01 +0800

    mac

--- 01_ddpm/02_train_eval.py
+++ 01_ddpm/02_train_eval.py
@@ -16,19 +16,22 @@ from unet import UNet
 
 def train(config: Dict):
     device = torch.device(config["device"])
-    # dataset
+
     dataset = CIFAR10(root='../00_assets/datasets', train=True, download=True,
                       transform=Compose([ToTensor(), Lambda(lambda x: (x - 0.5) * 2)]))
-    dataloader = DataLoader(dataset, batch_size=config["batch_size"],
-                            shuffle=True, num_workers=4, drop_last=True, pin_memory=True)
+    dataloader = DataLoader(dataset, batch_size=config["batch_size"], shuffle=True, drop_last=True, pin_memory=True)
+    _, batch = next(enumerate(dataloader))
+    img_batch = torch.clamp(batch[0] * 0.5 + 0.5, 0, 1)
+    save_image(img_batch, os.path.join(config["image_dir"], config["raw_name"]), nrow=10)
 
-    # model setup
-    net_model = UNet(channel_img=3, channel_base=config["channel"], channel_mults=config["channel_mult"],
-                     num_res_blocks=config["num_res_blocks"], dropout=config["dropout"]).to(device)
-    if config["training_load_weight"] is not None:
+    net_model = UNet(channel_img=config["img_channel"], channel_base=config["channel"],
+                     channel_mults=config["channel_mult"], num_res_blocks=config["num_res_blocks"],
+                     n_steps=config["T"], dropout=config["dropout"]).to(device)
+    if config["training_weight"] is not None:
         net_model.load_state_dict(torch.load(os.path.join(
-            config["save_dir"], config["training_load_weight"]), map_location=device), strict=False)
+            config["model_dir"], config["training_weight"]), map_location=device), strict=False)
         print("Model weight load down.")
+
     optimizer = torch.optim.AdamW(net_model.parameters(), lr=config["lr"], weight_decay=1e-4)
     cosineScheduler = optim.lr_scheduler.CosineAnnealingLR(
         optimizer=optimizer, T_max=config["epoch"], eta_min=0, last_epoch=-1)
@@ -36,70 +39,60 @@ def train(config: Dict):
                                              warm_epoch=config["epoch"] // 10, after_scheduler=cosineScheduler)
     trainer = GaussianDiffusionTrainer(net_model, config["beta_1"], config["beta_T"], config["T"]).to(device)
 
-    # start training
     for e in range(config["epoch"]):
         with tqdm(dataloader, dynamic_ncols=True) as tqdmDataLoader:
             for images, labels in tqdmDataLoader:
-                # train
-                b = images.shape[0]
                 optimizer.zero_grad()
+                b = images.shape[0]
                 x_0 = images.to(device)
-                labels = labels.to(device) + 1
+                labels = labels.to(device) + 1  # why
                 if np.random.rand() < 0.1:
                     labels = torch.zeros_like(labels).to(device)
                 loss = trainer(x_0, labels).sum() / b ** 2.
                 loss.backward()
-                torch.nn.utils.clip_grad_norm_(
-                    net_model.parameters(), config["grad_clip"])
+                torch.nn.utils.clip_grad_norm_(net_model.parameters(), config["grad_clip"])
                 optimizer.step()
                 tqdmDataLoader.set_postfix(ordered_dict={
                     "epoch": e,
                     "loss: ": loss.item(),
-                    "img shape: ": x_0.shape,
                     "LR": optimizer.state_dict()['param_groups'][0]["lr"]
                 })
         warmUpScheduler.step()
-        os.makedirs(config["save_dir"], exist_ok=True)
-        torch.save(net_model.state_dict(), os.path.join(config["save_dir"], 'ckpt_' + str(e) + ".pth"))
+        os.makedirs(config["model_dir"], exist_ok=True)
+        torch.save(net_model.state_dict(), os.path.join(config["model_dir"], 'ckpt_' + str(e) + ".pth"))
 
 
 def generate(config: Dict):
     device = torch.device(config["device"])
     # load model and evaluate
     with torch.no_grad():
-        step = int(config["batch_size"] // 10)
-        labelList = []
-        k = 0
-        for i in range(1, config["batch_size"] + 1):
-            labelList.append(torch.ones(size=[1]).long() * k)
-            if i % step == 0:
-                if k < 10 - 1:
-                    k += 1
-        labels = torch.cat(labelList, dim=0).long().to(device) + 1
+        values = torch.arange(1, 11)
+        labels = values.repeat_interleave(config["nrow"])
         print("labels: ", labels)
         model = UNet(channel_img=3, channel_base=config["channel"], channel_mults=config["channel_mult"],
                      num_res_blocks=config["num_res_blocks"], dropout=config["dropout"]).to(device)
-        ckpt = torch.load(os.path.join(config["save_dir"], config["test_load_weight"]), map_location=device)
+        ckpt = torch.load(os.path.join(config["model_dir"], config["eval_weight"]), map_location=device)
         model.load_state_dict(ckpt)
         print("model load weight done.")
         model.eval()
         sampler = GaussianDiffusionSampler(
-            model, config["beta_1"], config["beta_T"], config["T"], w=config["w"]).to(device)
-        # Sampled from standard normal distribution
-        noisyImage = torch.randn(
-            size=[config["batch_size"], 3, config["img_size"], config["img_size"]], device=device)
-        saveNoisy = torch.clamp(noisyImage * 0.5 + 0.5, 0, 1)
-        os.makedirs(config["sampled_dir"], exist_ok=True)
-        save_image(saveNoisy, os.path.join(config["sampled_dir"], config["sampledNoisyImgName"]), nrow=config["nrow"])
+            model, config["beta_1"], config["beta_T"], config["T"],
+            save_path=config["image_dir"] + "images/",
+            nrow=config["nrow"],
+            w=config["w"]).to(device)
+
+        noisyImage = torch.randn(size=[config["batch_size"], config["img_channel"],
+                                       config["img_size"], config["img_size"]], device=device)
+        saveNoisy = torch.clip(noisyImage * 0.5 + 0.5, 0, 1)
+        os.makedirs(config["image_dir"], exist_ok=True)
+        save_image(saveNoisy, os.path.join(config["image_dir"], config["noisy_name"]), nrow=config["nrow"])
         sampledImgs = sampler(noisyImage, labels)
         sampledImgs = sampledImgs * 0.5 + 0.5  # [0 ~ 1]
-        print(sampledImgs)
-        save_image(sampledImgs, os.path.join(config["sampled_dir"], config["sampledImgName"]), nrow=config["nrow"])
+        save_image(sampledImgs, os.path.join(config["image_dir"], config["generate_name"]), nrow=config["nrow"])
 
 
 if __name__ == '__main__':
     modelConfig = {
-        "state": "train",  # or eval
         "epoch": 70,
         "batch_size": 80,
         "T": 1000,
@@ -111,17 +104,19 @@ if __name__ == '__main__':
         "multiplier": 2.5,
         "beta_1": 1e-4,
         "beta_T": 0.028,
+        "img_channel": 3,
         "img_size": 32,
         "grad_clip": 1.,
-        "device": "cuda:0",
+        "device": "cpu",
         "w": 1.8,
-        "save_dir": "../00_assets/model_cifar10/",
-        "training_load_weight": None,
-        "test_load_weight": "ckpt_63.pth",
-        "sampled_dir": "../00_assets/img_cifar10/",
-        "sampledNoisyImgName": "NoisyGuidenceImgs.png",
-        "sampledImgName": "SampledGuidenceImgs.png",
+        "model_dir": "../00_assets/model_cifar10/",
+        "training_weight": None,
+        "eval_weight": "ckpt_63.pth",
+        "image_dir": "../00_assets/img_cifar10/",
+        "noisy_name": "img_noisy.png",
+        "generate_name": "img_generate.png",
+        "raw_name": "img_raw.png",
         "nrow": 8
     }
-    train(modelConfig)
+    # train(modelConfig)
     generate(modelConfig)
--- 01_ddpm/utils.py
+++ 01_ddpm/utils.py
@@ -1,13 +1,19 @@
-import matplotlib.pyplot as plt
-import numpy as np
-from tqdm import tqdm
-import imageio
-import einops
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
 from torch.optim.lr_scheduler import LRScheduler
+from torchvision.utils import save_image
+import os
+
+
+def extract(v, t, x_shape):
+    """
+    Extract some coefficients at specified timesteps, then reshape to
+    [batch_size, 1, 1, 1, 1, ...] for broadcasting purposes.
+    """
+    device = t.device
+    out = torch.gather(v, index=t, dim=0).float().to(device)
+    return out.view([t.shape[0]] + [1] * (len(x_shape) - 1))
 
 
 class GradualWarmupScheduler(LRScheduler):
@@ -41,16 +47,6 @@ class GradualWarmupScheduler(LRScheduler):
             return super(GradualWarmupScheduler, self).step(epoch)
 
 
-def extract(v, t, x_shape):
-    """
-    Extract some coefficients at specified timesteps, then reshape to
-    [batch_size, 1, 1, 1, 1, ...] for broadcasting purposes.
-    """
-    device = t.device
-    out = torch.gather(v, index=t, dim=0).float().to(device)
-    return out.view([t.shape[0]] + [1] * (len(x_shape) - 1))
-
-
 class GaussianDiffusionTrainer(nn.Module):
     def __init__(self, model, beta_1, beta_T, T):
         super().__init__()
@@ -59,29 +55,28 @@ class GaussianDiffusionTrainer(nn.Module):
         self.register_buffer('betas', torch.linspace(beta_1, beta_T, T).double())
         alphas = 1. - self.betas
         alphas_bar = torch.cumprod(alphas, dim=0)
-
-        # calculations for diffusion q(x_t | x_{t-1}) and others
         self.register_buffer('sqrt_alphas_bar', torch.sqrt(alphas_bar))
         self.register_buffer('sqrt_one_minus_alphas_bar', torch.sqrt(1. - alphas_bar))
 
     def forward(self, x_0, labels):
-        """
-        Algorithm 1.
-        """
         t = torch.randint(self.T, size=(x_0.shape[0],), device=x_0.device)
         noise = torch.randn_like(x_0)
-        x_t = extract(self.sqrt_alphas_bar, t, x_0.shape) * x_0 + \
-              extract(self.sqrt_one_minus_alphas_bar, t, x_0.shape) * noise
-        loss = F.mse_loss(self.model(x_t, t, labels), noise, reduction='none')
+        # extract 计算第t步加了噪音的图片，noisy_img
+        x_t = (extract(self.sqrt_alphas_bar, t, x_0.shape) * x_0 +
+               extract(self.sqrt_one_minus_alphas_bar, t, x_0.shape) * noise)
+        pred_noise = self.model(x_t, t, labels)
+        loss = F.mse_loss(pred_noise, noise, reduction='none')
         return loss
 
 
 class GaussianDiffusionSampler(nn.Module):
-    def __init__(self, model, beta_1, beta_T, T, w=0.):
+    def __init__(self, model, beta_1, beta_T, T, save_path, nrow, w=0.):
         super().__init__()
-
+        self.save_path = save_path
+        self.nrow = nrow
         self.model = model
         self.T = T
+
         """
         In the classifier free guidance paper, w is the key to control the guidance.
         w = 0 and with label = 0 means no guidance.
@@ -89,7 +84,6 @@ class GaussianDiffusionSampler(nn.Module):
         """
 
         self.w = w
-
         self.register_buffer('betas', torch.linspace(beta_1, beta_T, T).double())
         alphas = 1. - self.betas
         alphas_bar = torch.cumprod(alphas, dim=0)
@@ -113,9 +107,6 @@ class GaussianDiffusionSampler(nn.Module):
         return xt_prev_mean, var
 
     def forward(self, x_T, labels):
-        """
-        Algorithm 2.
-        """
         x_t = x_T
         for time_step in reversed(range(self.T)):
             print(time_step)
@@ -127,76 +118,8 @@ class GaussianDiffusionSampler(nn.Module):
                 noise = 0
             x_t = mean + torch.sqrt(var) * noise
             assert torch.isnan(x_t).int().sum() == 0, "nan in tensor."
+
+            img_save = torch.clip(x_t * 0.5 + 0.5, 0, 1)
+            save_image(img_save, os.path.join(self.save_path, f'{self.T - time_step:04d}.png'), nrow=self.nrow)
         x_0 = x_t
         return torch.clip(x_0, -1, 1)
-
-
-def show_images(images, title="sample"):
-    """Shows the provided images as sub-pictures in a square"""
-    if type(images) is torch.Tensor:
-        images = images.detach().cpu().numpy()
-    fig = plt.figure(figsize=(10, 10))
-    rows = int(len(images) ** (1 / 2))
-    cols = round(len(images) / rows)
-    idx = 0
-    for r in range(rows):
-        for c in range(cols):
-            if idx < len(images):
-                ax = fig.add_subplot(rows, cols, idx + 1)
-                image = images[idx].transpose((1, 2, 0))
-                image = (image / 2.0) + 0.5
-                ax.imshow(image)
-                ax.axis('off')
-                idx += 1
-    fig.suptitle(title, fontsize=30)
-    plt.savefig(title + '.png')
-
-
-def generate_new_images(ddpm, config, n_samples=100, frames_per_gif=10):
-    """Given a DDPM model, a number of samples to be generated and a device, returns some newly generated samples"""
-    frames = []
-    if config['dt'] == 'mnist':
-        c, h, w = 1, 28, 28
-    elif config['dt'] == 'cifar10':
-        c, h, w = 3, 32, 32
-    else:
-        raise ValueError("Unsupported dataset type")
-
-    with torch.no_grad():
-        x = torch.randn(n_samples, c, h, w).to(config['device'])
-        for idx, t in tqdm(enumerate(list(range(config['n_steps']))[::-1])):
-            t_tensor = torch.as_tensor([t] * n_samples).to(config['device'])
-            if config['with_class']:
-                y_tensor = torch.as_tensor([config['label']] * n_samples).to(config['device'])
-                eta_theta = ddpm(x, t_tensor, y_tensor)
-            else:
-                eta_theta = ddpm(x, t_tensor, None)
-
-            alpha_t = ddpm.alphas[t]
-            alpha_t_hat = ddpm.alphas_hat[t]
-            x = (1 / alpha_t.sqrt()) * (x - (1 - alpha_t) / (1 - alpha_t_hat).sqrt() * eta_theta)
-
-            if t > 0:
-                z = torch.randn(n_samples, c, h, w).to(config['device'])
-                beta_t = ddpm.betas[t]
-                sigma_t = beta_t.sqrt()
-                x = x + sigma_t * z
-
-            if idx % frames_per_gif == 0:
-                normalized = (x - x.min()) / (x.max() - x.min()) * 255
-                frame = einops.rearrange(normalized, "(b1 b2) c h w -> (b1 h) (b2 w) c", b1=int(n_samples ** 0.5))
-                frame = frame.cpu().numpy().astype(np.uint8)
-                frames.append(frame)
-
-    gif_path = f"../00_assets/ddpm_{config['dt']}_class_{config['label']}.gif"
-    with imageio.get_writer(gif_path, mode="I") as writer:
-        for idx, frame in enumerate(frames):
-            rgb_frame = np.repeat(frame, 3, axis=2)
-            writer.append_data(rgb_frame)
-
-            if idx == len(frames) - 1:
-                last_rgb_frame = np.repeat(frames[-1], 3, axis=2)
-                for _ in range(frames_per_gif // 3):
-                    writer.append_data(last_rgb_frame)
-
-    return x
